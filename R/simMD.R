6f1f7c4f1c6f29132c3eff581de1cdb07aa32cb3
HeidiChristensen <heidi@math.aau.dk>
2020-10-27 13:50:10 +0100

    Correction in simMD.R

--- R/simMD.R
+++ R/simMD.R
@@ -34,7 +34,7 @@ simMD <- function(subjects = 15, observers = 20, measurements = 1, mu = 0, sigma
 
     SigmaA <- sigma2A * kronecker(kronecker(diag(subjects), matrix(1, nrow = observers, ncol = observers)),
                                   matrix(1, nrow = measurements, ncol = measurements))
-    SigmaB <- sigma2B * kronecker(matrix(1, nrow = subjects, ncol = subjects), kronecker(diag(observers)),
+    SigmaB <- sigma2B * kronecker(kronecker(matrix(1, nrow = subjects, ncol = subjects), diag(observers)),
                                   matrix(1, nrow = measurements, ncol = measurements))
     SigmaE <- sigma2E * diag(subjects * observers * measurements)
 
