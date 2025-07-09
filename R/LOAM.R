a6dd003d2a49df470454fb1ae93e8caa15e4ac60
Heidi Søgaard Christensen <f1hh@aas.its.nja.dk>
2025-07-09 14:25:42 +0200

    Extended remaining functions to interaction term

--- R/LOAM.R
+++ R/LOAM.R
@@ -1,7 +1,10 @@
 #' Function LOAM 13-01-20
 #'
 #' @description This function calculates estimates and confidence intervals for the
-#' 95\% limits of agreement with the mean presented by \insertCite{christensen;textual}{loamr}.
+#' 95\% limits of agreement with the mean (LOAM) \insertCite{christensen;textual}{loamr}.
+#' It provides both the reproducibility LOAM and the repeatibility LOAM
+#' (when > 1 measurement per observer per subject).
+##'
 #'
 #' @details The data argument requires data in long/narrow format with
 #' the following columns:
@@ -20,14 +23,22 @@
 #' The procedure requires balanced data, meaning that all observers must have measured
 #' all subjects the same number of times.
 #'
-#' The function outputs estimates and approximate CIs for the LOAM, the
-#' intra-class correlation, \eqn{\sigma_A}, \eqn{\sigma_B}, and \eqn{\sigma_E}, where
-#' \eqn{\sigma_A^2}, \eqn{\sigma_B^2}, and \eqn{\sigma_E^2} are the variance components of the underlying
-#' two-way random model.  See \insertCite{christensen;textual}{loamr} for details.
+#' The function outputs estimates and CIs for the reproducibility LOAM under a
+#' two-way random effects model with or without interaction. Further, estimate
+#' and CIs for \eqn{\sigma_A}, \eqn{\sigma_B}, \eqn{\sigma_AB} (if interaction included)
+#' and \eqn{\sigma_E} are supplied, where  \eqn{\sigma_A^2} is the subject variance,
+#' \eqn{\sigma_B^2} the observer variance, ' \eqn{\sigma_AB^2} the
+#' observer-sujebct interaction variance, and \eqn{\sigma_E^2} the residual variance.
+#' If >1 measurement per observer per subject, estimate and CI for repeatibility
+#' LOAM is supplied.
+#' If only one measurement per observer per subject, estimate and CI for the
+#' intra-class correlation ICC(A, 1) is supplied.
+#' See \insertCite{christensen;textual}{loamr} for details.
 #'
 #'
 #' @param data a data frame containing measurement data in long format (see 'Details')
-#' @param CI coverage probability for the confidence interval on the LOAM.
+#' @param CI.coverage coverage probability for the confidence interval on the LOAM.
+#' @param interaction logical, indicates if subject-observer interaction should be included in the two-way random effects model
 #'
 #' @references
 #' \insertRef{christensen}{loamr}
@@ -51,7 +62,7 @@
 #' @importFrom rlang .data
 
 
-LOAM <- function(data, CI = 0.95) {
+LOAM <- function(data, interaction = F, CI.coverage = 0.95) {
 
   if (!(tibble::has_name(data, "measurement"))) {
     data$measurement <- as.integer(1)
@@ -59,19 +70,28 @@ LOAM <- function(data, CI = 0.95) {
 
   LOAM_perc <- 0.95
   z  <- abs(qnorm((1 - LOAM_perc) / 2))
-  z2 <- abs(qnorm((1 - CI) / 2))
-  up <- 1 - (1 - CI) / 2
-  lo <-     (1 - CI) / 2
+  z2 <- abs(qnorm((1 - CI.coverage) / 2))
+
+  up <- 1 - (1 - CI.coverage) / 2
+  lo <-     (1 - CI.coverage) / 2
 
   a <- length(unique(data$subject))
   b <- length(unique(data$observer))
-  c <- length(unique(data$measurement))
-  N <- a * b * c
+  h <- length(unique(data$measurement))
+  N <- a * b * h
 
-  vE <- a * b * c - a - b + 1
   vA <- a - 1
   vB <- b - 1
 
+  if(interaction){
+    if(h == 1) stop("Need >1 measurement per observer per subject when interaction = T")
+    vAB <- vA * vB
+    vE  <- N - a * b
+  } else{
+    vE <- N - a - b + 1
+  }
+
+  # Sums of squares
   da <- data %>%
     group_by(.data$observer) %>%
     mutate(observerMean = mean(.data$value)) %>%
@@ -79,27 +99,130 @@ LOAM <- function(data, CI = 0.95) {
     group_by(.data$subject) %>%
     mutate(subjectMean = mean(.data$value)) %>%
     ungroup() %>%
+    group_by(.data$subject, .data$observer) %>%
+    mutate(subjectobserverMean = mean(.data$value)) %>%
+    ungroup %>%
     mutate(valueMean = mean(.data$value))
 
-  SSE <- sum((da$value - da$subjectMean - da$observerMean + da$valueMean)^2)
   SSA <- sum((da$subjectMean - da$valueMean)^2)
   SSB <- sum((da$observerMean - da$valueMean)^2)
+  if(interaction){
+    SSAB <- sum((da$subjectobserverMean - da$valueMean)^2) - SSA - SSB
+    MSAB <- SSAB / vAB  #
 
-  MSE <- SSE / vE
-  MSA <- SSA / vA
-  MSB <- SSB / vB
+    SSE  <- sum((da$value - da$subjectobserverMean)^2)
+  } else{
+    SSE <- sum((da$value - da$subjectMean - da$observerMean + da$valueMean)^2)
+  }
 
-  sigma2E <-  MSE
-  sigma2A <- (MSA - MSE) / (b * c)
-  sigma2B <- (MSB - MSE) / (a * c)
+  MSE  <- SSE / vE
+  MSA  <- SSA / vA
+  MSB  <- SSB / vB
 
-  sigmaE <- sqrt(sigma2E)
+  # Variance estimates
+  if(interaction){
+    sigma2A  <- (MSA - MSAB) / (b * h)
+    sigma2B  <- (MSB - MSAB) / (a * h)
+
+    sigma2AB <- (MSAB - MSE) / h
+    sigmaAB <- ifelse(sigma2AB >= 0, sqrt(sigma2AB), NA)
+
+  } else{
+    sigma2A <- (MSA - MSE) / (b * h)
+    sigma2B <- (MSB - MSE) / (a * h)
+    sigmaAB <- "-"
+  }
+
   sigmaA <- ifelse(sigma2A >= 0, sqrt(sigma2A), NA)
   sigmaB <- ifelse(sigma2B >= 0, sqrt(sigma2B), NA)
 
-  LOAM <- z * sqrt((SSB + SSE) / N)
+  sigma2E <-  MSE
+  sigmaE <- sqrt(sigma2E)
 
-  if (c == 1 & sigma2A >= 0) {
+  # LOAM estimates
+  if(h > 1){
+    LOAM_repeat <- z * sqrt((h - 1) / h * sigma2E)
+  } else{
+    LOAM_repeat <- "-"
+  }
+
+  if(interaction){
+    LOAM_reprod <- z * sqrt((SSB + SSAB + SSE) / N)
+  } else{
+    LOAM_reprod <- z * sqrt((SSB + SSE) / N)
+  }
+
+  # Reproducibility LOAM CI
+  lB <- 1 - 1 / qf(up, vB, Inf)
+  hB <- 1     / qf(lo, vB, Inf) - 1
+  lE <- 1 - 1 / qf(up, vE, Inf)
+  hE <- 1     / qf(lo, vE, Inf) - 1
+
+  if(interaction){
+    lAB <- 1 - 1 / qf(up, vAB, Inf)
+    hAB <- 1     / qf(lo, vAB, Inf) - 1
+    H <- sqrt(hB^2 * SSB^2 + hAB^2 * SSAB^2 + hE^2 * SSE^2)
+    L <- sqrt(lB^2 * SSB^2 + lAB^2 * SSAB^2 + lE^2 * SSE^2)
+
+    reprod_CI <- c(z * sqrt((SSB + SSAB + SSE - L) / N),
+                   z * sqrt((SSB + SSAB + SSE + H) / N))
+
+  } else{
+    H <- sqrt(hB^2 * SSB^2 + hE^2 * SSE^2)
+    L <- sqrt(lB^2 * SSB^2 + lE^2 * SSE^2)
+
+    reprod_CI <- c(z * sqrt((SSB + SSE - L) / N),
+                   z * sqrt((SSB + SSE + H) / N))
+  }
+
+  # Repeatibility LOAM CI
+  if(h > 1){
+    repeat_CI <- c(z * sqrt( (h - 1) * SSE / (h * qchisq(up, vE))),
+                   z * sqrt( (h - 1) * SSE / (h * qchisq(lo, vE))))
+  } else{
+    repeat_CI <- NA
+  }
+
+  # CI for variance components
+  if(interaction){
+    var1 <- h * sigma2AB + sigma2E
+    v <- vAB
+  } else{
+    var1 <- sigma2E
+    v <- vE
+  }
+
+  if(sigma2A >= 0){
+    sigmaA_CI <- sigmaA + c(-1, 1) * z2 / (b * h * sigmaA) * sqrt((b * h * sigma2A + var1)^2 / (2 * vA) + var1^2 / (2 * v))
+  } else {
+    sigmaA_CI <- NA
+    warning("Estimate of sigma2A < 0.")
+  }
+
+  if(sigma2B >= 0){
+    sigmaB_CI <- sigmaB + c(-1, 1) * z2 / (a * h * sigmaB) * sqrt((a * h * sigma2B + var1)^2 / (2 * vB) + var1^2 / (2 * v))
+  } else {
+    sigmaB_CI <- NA
+    warning("Estimate of sigma2B < 0.")
+  }
+
+  if(interaction){
+    if(sigma2AB >= 0){
+      sigmaAB_CI <- sigmaAB + c(-1, 1) * z2 / (h * sigmaAB) * sqrt(var1^2 / (2 * vAB) + (sigma2E ^ 2 / (2 * vE)))
+    } else {
+      sigmaAB_CI <- NA
+      warning("Estimate of sigma2AB < 0.")
+    }
+  } else{
+    sigmaAB_CI <- NA
+  }
+
+  sigmaE_CI <- c(sigmaE * sqrt(vE / qchisq(up, vE)),
+                 sigmaE * sqrt(vE / qchisq(lo, vE)))
+
+
+  # Intra-class correlation coefficient estimate and CI
+  if (h == 1 & sigma2A >= 0) {
     ICC       <- sigma2A / (sigma2A + sigma2B + sigma2E)
     A         <- b * ICC / (a * (1 - ICC))
     B         <- 1 + b * ICC * (a - 1) / (a * (1 - ICC))
@@ -112,47 +235,20 @@ LOAM <- function(data, CI = 0.95) {
     upp_denom <- b * MSB + (a * b - a - b) * MSE + a * FU * MSA
     ICC_CI    <- c(low_num / low_denom, upp_num / upp_denom)
   } else {
-    ICC       <- NA
-    ICC_CI    <- NA
+    ICC       <- "-"
+    ICC_CI    <- "-"
   }
 
-  if(sigma2A >= 0){
-    sigmaA_CI <- sigmaA + (c(-1, 1) * ((z2 / b) * sqrt((1 / (2 * sigma2A)) * (((b * sigma2A + sigma2E)^2 / vA)) + (sigma2E^2 / vE))))
-  } else {
-    sigmaA_CI <- NA
-    warning("Estimate of sigma2A < 0.")
-  }
-
-  if(sigma2B >= 0){
-    sigmaB_CI <- sigmaB + (c(-1, 1) * ((z2 / a) * sqrt((1 / (2 * sigma2B)) * (((a * sigma2B + sigma2E)^2 / vB)) + (sigma2E^2 / vE))))
-  } else {
-    sigmaB_CI <- NA
-    warning("Estimate of sigma2B < 0.")
-  }
-
-  sigmaE_CI <- c(sigmaE * sqrt(vE / qchisq(up, vE)),
-                 sigmaE * sqrt(vE / qchisq(lo, vE)))
-
-
-  lB <- 1 - 1 / qf(up, vB, Inf)
-  hB <- 1     / qf(lo, vB, Inf) - 1
-  lE <- 1 - 1 / qf(up, vE, Inf)
-  hE <- 1     / qf(lo, vE, Inf) - 1
-
-  H <- sqrt(hB^2 * SSB^2 + hE^2 * SSE^2)
-  L <- sqrt(lB^2 * SSB^2 + lE^2 * SSE^2)
-
-  LOAM_CI <- c(z * sqrt((SSB + SSE - L) / N),
-               z * sqrt((SSB + SSE + H) / N))
-
-  result <- list(data      = da,
-                 estimates = data.frame(LOAM, sigmaA, sigmaB, sigmaE, ICC),
-                 intervals = data.frame(LOAM_CI, sigmaA_CI,
-                                        sigmaB_CI, sigmaE_CI,  ICC_CI),
-                 CI        = CI)
+  result <- list(data        = da,
+                 estimates   = data.frame(LOAM_reprod, LOAM_repeat, sigmaA,
+                                          sigmaB, sigmaAB, sigmaE, ICC),
+                 intervals   = data.frame(reprod_CI,  repeat_CI,
+                                          sigmaA_CI,  sigmaB_CI,
+                                          sigmaAB_CI, sigmaE_CI,
+                                          ICC_CI),
+                 CI.coverage = CI.coverage)
 
   class(result) <- "loamobject"
-
   return(result)
 
 }
--- R/generic_functions.R
+++ R/generic_functions.R
@@ -7,29 +7,35 @@
 #' @export
 #'
 
-
-
 print.loamobject <- function(x, digits = 3, ...) {
   fm <- function(x) {format(round(x, digits), nsmall = digits)}
 
   cat("Limits of agreement with the mean for multiple observers", sep = "")
   cat("\n\n")
   cat("The data has", nrow(x$data),"observations from", length(unique(x$data$subject)),
-      "individuals by", length(unique(x[[1]]$observer)), "observers with", length(unique(x[[1]]$measurement)),"measurements")
+      "individuals by", length(unique(x[[1]]$observer)), "observers with", length(unique(x[[1]]$measurement)),"repeated measurements")
   cat("\n\n")
-  cat("LOAM:  +/- ", fm(x$estimates$LOAM), " (", fm(x$intervals$LOAM_CI[1]), ", ", fm(x$intervals$LOAM_CI[2]), ")", sep = "")
+  cat("95% reproducibility LOAM:  +/- ", fm(x$estimates$LOAM_reprod), " (", fm(x$intervals$reprod_CI[1]), ", ", fm(x$intervals$reprod_CI[2]), ")", sep = "")
+  if(!(x$estimates$LOAM_repeat == "-")){
+    cat("\n")
+    cat("95% repeability LOAM:      +/- ", fm(x$estimates$LOAM_repeat), " (", fm(x$intervals$repeat_CI[1]), ", ", fm(x$intervals$repeat_CI[2]), ")", sep = "")
+  }
   cat("\n\n")
-  cat("sigmaA:    ", fm(x$estimates$sigmaA), " (", fm(x$intervals$sigmaA_CI[1]), ", ", fm(x$intervals$sigmaA_CI[2]), ")", sep = "")
+  cat("sigmaA:    ", fm(x$estimates$sigmaA),  " (", fm(x$intervals$sigmaA_CI[1]),  ", ", fm(x$intervals$sigmaA_CI[2]),  ")", sep = "")
   cat("\n")
-  cat("sigmaB:    ", fm(x$estimates$sigmaB), " (", fm(x$intervals$sigmaB_CI[1]), ", ", fm(x$intervals$sigmaB_CI[2]), ")", sep = "")
+  cat("sigmaB:    ", fm(x$estimates$sigmaB),  " (", fm(x$intervals$sigmaB_CI[1]),  ", ", fm(x$intervals$sigmaB_CI[2]),  ")", sep = "")
+  if(!(x$estimates$sigmaAB == "-")){
+    cat("\n")
+    cat("sigmaAB:   ", fm(x$estimates$sigmaAB), " (", fm(x$intervals$sigmaAB_CI[1]), ", ", fm(x$intervals$sigmaAB_CI[2]), ")", sep = "")
+  }
   cat("\n")
-  cat("sigmaE:    ", fm(x$estimates$sigmaE), " (", fm(x$intervals$sigmaE_CI[1]), ", ", fm(x$intervals$sigmaE_CI[2]), ")", sep = "")
+  cat("sigmaE:    ", fm(x$estimates$sigmaE),  " (", fm(x$intervals$sigmaE_CI[1]),  ", ", fm(x$intervals$sigmaE_CI[2]),  ")", sep = "")
   cat("\n")
-  if (!is.na(x$estimates$ICC)) {
+  if (!(x$estimates$ICC == "-")) {
     cat("ICC(A,1):  ", fm(x$estimates$ICC), " (", fm(x$intervals$ICC_CI[1]), ", ", fm(x$intervals$ICC_CI[2]), ")", sep = "")
   }
   cat("\n\n")
-  cat("Coverage probability for the above CIs: ", x$CI * 100,"%", sep="")
+  cat("Coverage probability for the above CIs: ", x$CI.coverage * 100,"%", sep="")
   cat("\n")
 }
 
@@ -38,7 +44,7 @@ print.loamobject <- function(x, digits = 3, ...) {
 #' @param x an object of class 'loamobject'
 #' @param ... passthrough
 
-#' @return An agreement plot with an estimate and CI for the 95\% limits of agreement with the mean
+#' @return An agreement plot with an estimate and CI for the 95\% reproducibility limits of agreement with the mean
 #' @import ggplot2
 #' @importFrom rlang .data
 #' @export
@@ -46,7 +52,8 @@ print.loamobject <- function(x, digits = 3, ...) {
 
 plot.loamobject <- function(x, ...) {
 
-  ci <- x$intervals$LOAM_CI
+  ci  <- x$intervals$reprod_CI
+  est <- x$estimates$LOAM_reprod
 
   fm <- function(x) {format(round(x, 3), nsmall = 3)}
   k <- max(x$data$measurement)
@@ -59,9 +66,9 @@ plot.loamobject <- function(x, ...) {
       ggplot(aes(x = .data$subjectMean, y = .data$centered, shape = as.factor(.data$observer))) +
       scale_shape_manual(values = 0:(b-1)) +
       geom_vline(aes(xintercept = .data$subjectMean), alpha = 0.1) +
-      geom_hline(yintercept =  x$estimates$LOAM, color = "#1f78b4", linetype = "dashed")+
-      geom_hline(yintercept = -x$estimates$LOAM, color = "#1f78b4", linetype = "dashed")+
-      annotate("rect", ymin = ci[1],  ymax =  ci[2], xmin = -Inf, xmax = Inf, alpha = 0.2, fill = "#1f78b4") +
+      geom_hline(yintercept =  est, color = "#1f78b4", linetype = "dashed")+
+      geom_hline(yintercept = -est, color = "#1f78b4", linetype = "dashed")+
+      annotate("rect", ymin =  ci[1], ymax =  ci[2], xmin = -Inf, xmax = Inf, alpha = 0.2, fill = "#1f78b4") +
       annotate("rect", ymin = -ci[1], ymax = -ci[2], xmin = -Inf, xmax = Inf, alpha = 0.2, fill = "#1f78b4") +
       geom_point(size = 2, ...) +
       theme_bw() + theme(panel.grid.major = element_blank(),
@@ -69,7 +76,7 @@ plot.loamobject <- function(x, ...) {
       labs(x = ifelse(k > 1, expression(italic(bar(y)[i..])), expression(italic(bar(y)[i.]))),
            y = ifelse(k > 1, expression(italic(y[ijk] - bar(y)[i..])), expression(italic(y[ij] - bar(y)[i.]))),
            title = "Agreement plot",
-           subtitle = paste0("LOAM +/- ", fm(x$estimates$LOAM), "   ", x$CI*100, "% CI: (", fm(ci[1]),", ",fm(ci[2]),")"),
+           subtitle = paste0("95% reproducbility LOAM +/- ", fm(est), " with ", x$CI.coverage*100, "% CI: (", fm(ci[1]),", ",fm(ci[2]),")"),
            shape = "Observer")
 
   } else {
@@ -80,8 +87,8 @@ plot.loamobject <- function(x, ...) {
       mutate(centered = .data$value - .data$subjectMean) %>%
       ggplot(aes(x = .data$subjectMean, y = .data$centered)) +
       geom_vline(aes(xintercept = .data$subjectMean), alpha=0.1) +
-      geom_hline(yintercept =  x$estimates$LOAM, color = "#1f78b4", linetype = "dashed")+
-      geom_hline(yintercept = -x$estimates$LOAM, color = "#1f78b4", linetype = "dashed")+
+      geom_hline(yintercept =  est, color = "#1f78b4", linetype = "dashed")+
+      geom_hline(yintercept = -est, color = "#1f78b4", linetype = "dashed")+
       annotate("rect", ymin = ci[1],  ymax =  ci[2], xmin = -Inf, xmax = Inf, alpha = 0.2, fill = "#1f78b4") +
       annotate("rect", ymin = -ci[1], ymax = -ci[2], xmin = -Inf, xmax = Inf, alpha = 0.2, fill = "#1f78b4") +
       geom_point(size = 2, alpha = 0.5, ...) +
@@ -90,7 +97,7 @@ plot.loamobject <- function(x, ...) {
       labs(x = ifelse(k > 1, expression(italic(bar(y)[i..])), expression(italic(bar(y)[i.]))),
            y = ifelse(k > 1, expression(italic(y[ijk] - bar(y)[i..])), expression(italic(y[ij] - bar(y)[i.]))),
            title = "Agreement plot",
-           subtitle = paste0("LOAM +/- ", fm(x$estimates$LOAM), "   ", x$CI*100, "% CI: (", fm(ci[1]),", ",fm(ci[2]),")"),
+           subtitle = paste0("95% reproducbility LOAM +/- ", fm(est), " with ", x$CI.coverage*100, "% CI: (", fm(ci[1]),", ",fm(ci[2]),")"),
            shape = "Observer")
   }
 }
